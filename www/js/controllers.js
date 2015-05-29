0ee6810abf8f7c53819a3122a7529f58ed4d7b96
Dongmin (Reo) Lee <reoim10@gmail.com>
2015-05-29 15:52:40 +1000

    img view added

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -318,6 +318,9 @@ angular.module('fieldAgent.controllers', [])
             status: ''
         }
 
+
+
+
         $ionicModal.fromTemplateUrl('contact-modal.html', {
             scope: $scope,
             animation: 'slide-in-up'
@@ -331,6 +334,8 @@ angular.module('fieldAgent.controllers', [])
 
         $scope.closeModal = function() {
             $scope.modal.hide();
+
+
         };
 
         $scope.$on('$destroy', function() {
@@ -340,27 +345,7 @@ angular.module('fieldAgent.controllers', [])
         $scope.getPhoto = function() {
             Camera.getPicture().then(function(imageURI) {
                 $scope.lastPhoto = imageURI;
-                var MAX_WIDTH = 50;
-                var MAX_HEIGHT = 50;
-                var width = img.width;
-                var height = img.height;
 
-                if (width > height) {
-                    if (width > MAX_WIDTH) {
-                        height *= MAX_WIDTH / width;
-                        width = MAX_WIDTH;
-                    }
-                } else {
-                    if (height > MAX_HEIGHT) {
-                        width *= MAX_HEIGHT / height;
-                        height = MAX_HEIGHT;
-                    }
-                }
-                canvas.width = width;
-                canvas.height = height;
-                var ctx = canvas.getContext("2d");
-                ctx.drawImage(imageURI, 0, 0, width, height);
-                $scope.resizedPhoto = ctx.toDataURL();
             }, function(err) {
                 console.err(err);
             }, {
@@ -373,3 +358,26 @@ angular.module('fieldAgent.controllers', [])
         };
 
     })
+
+
+//var MAX_WIDTH = 50;
+//var MAX_HEIGHT = 50;
+//var width = img.width;
+//var height = img.height;
+//
+//if (width > height) {
+//    if (width > MAX_WIDTH) {
+//        height *= MAX_WIDTH / width;
+//        width = MAX_WIDTH;
+//    }
+//} else {
+//    if (height > MAX_HEIGHT) {
+//        width *= MAX_HEIGHT / height;
+//        height = MAX_HEIGHT;
+//    }
+//}
+//canvas.width = width;
+//canvas.height = height;
+//var ctx = canvas.getContext("2d");
+//ctx.drawImage(imageURI, 0, 0, width, height);
+//$scope.resizedPhoto = ctx.toDataURL();
